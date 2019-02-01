8d7f358b661a02f6a233661b55ff8c1968cda13e
Art Bachmann <bachmann.art@gmail.com>
2019-02-01 13:00:41 +0200

    classes finished

--- class-based-components/src/index.js
+++ class-based-components/src/index.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
  class App extends Component {
+
   render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    )      
     return (
       <div>
-        Hi there
+        Latitude:
       </div>
     )
   }
